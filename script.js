e0ba5894fe8a157ee0b617d7de4764818e85d9fa
mahendrajat1996 <130475100+mahendrajat1996@users.noreply.github.com>
2023-07-03 22:21:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateTime() {
+        const timerElement = document.getElementById("timer");
+        const currentDate = new Date();
+        const timeString = currentDate.toLocaleTimeString();
+        const dateString = currentDate.toLocaleDateString();
+        const dateTimeString = `${timeString} - ${dateString}`;
+        timerElement.textContent = dateTimeString;
+      }
+
+      // Update the time every second
+      setInterval(updateTime, 1000);
